52b93ad81d95aec852c43049070b835abc3397b6
djimenez <djimenez@S1-15>
2017-02-21 13:05:27 +0100

    ejecicio resuelto

--- Random​ EnemyGenerator/Random​ EnemyGenerator.cpp
+++ Random​ EnemyGenerator/Random​ EnemyGenerator.cpp
@@ -3,40 +3,65 @@
 using namespace std;
 
 
-enum class EnemyType { zombie, vampire, ghost, witch };
+enum class EnemyType { ZOMBIE, VAMPIRE, GHOST, WITCH,MAX };
 
 struct Enemy {
 
 	EnemyType type;
 	string name;
 	int health;
+
+	string getEnemyTypeString()
+	{
+		switch (type) {
+		case EnemyType::ZOMBIE: return "zombie";
+		case EnemyType::VAMPIRE: return "vampire";
+		case EnemyType::GHOST: return "ghost";
+		case EnemyType::WITCH: return "witch";
+		default: return"";
+
+
+
+
+		}
+
+
+
+	}
 				
 };
 
-void CreateRandomEnemy(string names[],Enemy enemies[], int &e) {
 
+bool operator== (const Enemy &a, const Enemy &b) {
 
+	return a.name == b.name && a.type == b.type;
 
-	
 }
 
-bool operator== (Enemy a, Enemy b) {
-	if (a == b)
-	
-		return true;
+static Enemy CreateRandomEnemy() {
+	static const int MAX_LIFE{ 500 };
+	static const string NAMES[]{ "Gertrudiz","Pancracia","Anacleto","Hipo","Eusa","Fush" };
+	return Enemy{
+		static_cast<EnemyType>(rand()%static_cast<int>(EnemyType ::MAX)),NAMES[rand()%(sizeof NAMES/sizeof string)],rand()% MAX_LIFE
 
-	else
-		return false;
+	};
+
 
 }
 
 int main() {
-	srand(time(NULL));
-	string names[10] = {"David","Marta","Dani","Alex","Maria","Ramon","Alessandro","Sara","Angel","Oscar"};
-	Enemy enemies[5];
-	int EnemiesCreated = 0; 
-	CreateRandomEnemy(names,enemies);
+	srand(static_cast<unsigned>(time(nullptr)));
+	const int MAX_ENEMIES{ 5 };
+	Enemy enemies[MAX_ENEMIES];
+	
+	int i{ 0 };
+	while (i < MAX_ENEMIES)
+	{
+		enemies [i]
 
+
+
+	}
 
 
 
